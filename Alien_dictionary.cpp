fbefeb79dee272fe862129138e5b50ba1752c6f8
nidhiKesarwani <kesarnidhi10@gmail.com>
2019-09-24 19:53:37 +0530

    Update Alien_dictionary.cpp

--- Alien_dictionary.cpp
+++ Alien_dictionary.cpp
@@ -1,58 +1,3 @@
-
-#include<bits/stdc++.h>
-using namespace std;
-string printOrder(string [], int , int );
-string order;
-bool f(string a,string b)
-{
-	int p1=0;int p2=0;
-	for(int i=0;i<min(a.size(),b.size()) and p1 ==p2;i++ )
-	{
-		p1 = order.find(a[i]);
-		p2 = order.find(b[i]);
-	//	cout<<p1<<" "<<p2<<endl;
-	}
-	
-	if(p1 == p2 and a.size()!=b.size())
-	return a.size()<b.size();
-	
-	return p1<p2;
-}
-	
-// Driver program to test above functions
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n,k;
-        cin>>n>>k;
-    string s[n];
-    for(int i=0;i<n;i++)cin>>s[i];
-    
-        string ss = printOrder(s,n,k);
-        order="";
-        for(int i=0;i<ss.size();i++)
-        order+=ss[i];
-        
-        string temp[n];
-        std::copy(s, s + n, temp);
-        sort(temp,temp+n,f);
-        
-        bool f= true;
-        for(int i=0;i<n;i++)
-        if(s[i]!=temp[i])f=false;
-        
-        cout<<f;
-        cout<<endl;
-        
-    }
-	return 0;
-}
-
-
-
 void topo(vector<int> graph[], int vis[], int v, vector<int>* a)
 {
     vis[v]=1;
@@ -69,6 +14,7 @@ void topo(vector<int> graph[], int vis[], int v, vector<int>* a)
 string printOrder(string dict[], int N, int K)
 {
    //Your code here
+   // N number of strings, K number of alphabets
    vector<int> graph[26];
    
    for(int i=0; i<N-1;i++)
